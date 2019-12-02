0f845ef04a4fd37922ff3551d76beb27d3eef6cc
Shakir <azimlishakir@gmail.com>
2019-12-02 16:57:03 +0400

    s
    s

--- demo/src/main/java/com/springcrud/demo/controller/UsersController.java
+++ demo/src/main/java/com/springcrud/demo/controller/UsersController.java
@@ -1,12 +1,13 @@
 package com.springcrud.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class UsersController {
-    @RequestMapping("/")
+    @RequestMapping("/login")
     private String UserController(){
-            return "index";
+            return "login";
     }
 }
